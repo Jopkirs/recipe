27236f969ab1deda1f0e1c780efc9d927f85dea5
Jopkirs <megaalexs@yahoo.com>
2019-03-15 10:46:27 +0300

    Added Ingredient and UnitOfMeasure entities

---
+++ src/main/java/com/kaigo/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package com.kaigo.recipe.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private String uof;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUof() {
+        return uof;
+    }
+
+    public void setUof(String uof) {
+        this.uof = uof;
+    }
+}
